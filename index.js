d23af557e66b3e7e4886e412a29f6d938f784ac9
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-06-02 21:50:04 +0600

    loading foods from single database collection

--- index.js
+++ index.js
@@ -22,28 +22,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const breakfastCollection = client
-      .db("mealCollection")
-      .collection("breakfasts");
-    const lunchCollection = client.db("mealCollection").collection("lunches");
-    const dinnerCollection = client.db("mealCollection").collection("dinners");
+    const cartCollection = client.db("mealCollection").collection("cart");
+    const foodCollection = client.db("mealCollection").collection("foods");
     //Load Breakfast API
     app.get("/breakfasts", async (req, res) => {
-      const query = {};
-      const cursor = breakfastCollection.find(query);
+      const query = { catagory: "breakfast" };
+      const cursor = foodCollection.find(query);
       const breakfasts = await cursor.toArray();
       res.send(breakfasts);
     });
 
     app.get("/lunches", async (req, res) => {
-      const query = {};
-      const cursor2 = lunchCollection.find(query);
+      const query = { catagory: "lunch" };
+      const cursor2 = foodCollection.find(query);
       const lunches = await cursor2.toArray();
       res.send(lunches);
     });
     app.get("/dinners", async (req, res) => {
-      const query = {};
-      const cursor3 = dinnerCollection.find(query);
+      const query = { catagory: "dinner" };
+      const cursor3 = foodCollection.find(query);
       const dinners = await cursor3.toArray();
       res.send(dinners);
     });
@@ -58,8 +55,16 @@ async function run() {
     //Delete Items
     app.delete("/delete/:id", async (req, res) => {
       const id = req.params.id;
+      console.log(id);
       const query = { _id: ObjectId(id) };
       const result = await breakfastCollection.deleteOne(query);
+      res.send(result);
+    });
+    //Adding to cart
+    app.post("/cart/:email", async (req, res) => {
+      const email = req.body.email;
+      const item = req.body;
+      const result = await cartCollection.insertOne(item);
       res.send(result);
     });
   } finally {
